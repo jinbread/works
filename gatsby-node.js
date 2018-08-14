3f9051f22bf09720db7e61ded3d2a2194c46774e
Jinjae Lee <jinjae.lee@icloud.com>
2018-08-15 00:11:44 +0200

    Recovered.

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,13 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-exports.modifyWebpackConfig = ({ config, stage }) => {
-  if (stage === "build-html") {
-    config.loader("null", {
-      test: /bad-module/,
-      loader: "null-loader",
-    });
-  }
-};
-
  // You can delete this file if you're not using it
