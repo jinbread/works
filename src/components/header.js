c7f31b2f9d5f4ab573d3ea3c4f6cbb062785e4cc
jinbread <jinjae.lee@icloud.com>
2018-08-15 10:39:42 +0200

    Header.css updated

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,19 @@ import './Header.css'
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      {/* <Link to="/"><img width="30" /></Link> */}
-      <Link to="/">Jinbread.works</Link>
-      <Link to="/works">Works</Link>
-      <Link to="/play">Play</Link>
-      <Link to="/write">Write</Link>
-      <Link to="/about">About</Link>
-      {/* <Link to="/buy"><button>Buy</button></Link>  */}
+      <div className="logo">
+        <Link to="/">Jinbread.works</Link>
+      </div>
+      <div className="MenuGroup">
+          <div className="MenuDetail">
+            <Link to="/works">Works</Link>
+            <Link to="/play">Play</Link>
+            <Link to="/write">Write</Link>
+            <Link to="/about">About</Link>
+          </div>
+          
+      </div>
+
   </div>
 </div>
 )
